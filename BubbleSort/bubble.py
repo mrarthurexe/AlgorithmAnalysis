d9fe51b41a4085dc43cb0521f095574eaca23bba
MrArthur_exe <contato.arthurmorais.dev@gmail.com>
2023-01-02 16:35:41 -0300

    atualizando

--- BubbleSort/bubble.py
+++ BubbleSort/bubble.py
@@ -1,21 +1,11 @@
-from array import array
-# Bubble sort algorithm
-array = [7, 3, 6, 2, 0]
-# Introduzida variável "tamanho" recebendo o tamanho do array.
-tamanho = len(array)
-# Loop para percorrer o array.
-for i in range(tamanho -1):
-    # Trocado indica se a posição foi trocada.
-    trocado = False
-    # For loop percorre "j" que é o tamanho do array - i.
-    for j in range (tamanho -i -1):
-        #Se o valor de j for maior que o valor de j+1, troca os valores.
-        if array[j] > array[j + 1]:
-            array[j], array[j + 1] = array[j + 1], array[j]
-            #Se a condição de troca é confirmada, troca o valor de trocado para True.
-            trocado = True
-    #Se o valor de trocado for False, o array está ordenado.
-    if not trocado:
-        break
+# obs: Este algoritmo de ordenação não é o mais adequado para grande parte dos casos.
+def bubble_sort(array):
+    # Percorremos o array(i)
+    for i in range(len(array)):
+    # Percorremos o array novamente(j)
+        for j in range(len(array)):
+            # Se o elemento atual(i) é maior que o próximo(j + 1), trocamos de lugar um pelo outro.
+            if array[j] > array[j+1]:
+            array[j], array[j+1] = array[j+1], array[j]
 
-print(array)
+print(bubble_sort([5, 3, 2, 8, 1, 4]))
